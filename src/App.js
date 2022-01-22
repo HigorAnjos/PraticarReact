024075f1e3d621c92968f07692a096d389514445
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-01-22 20:56:51 -0300

    card

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ function App() {
     <div className="App">
       <Hello />
 
-      <Card titulo="Desafio Aleatorio" >
-        <Aleatorio min={1} max={60} />
-      </Card>
+      <div className="Cards">
 
+        <Card titulo="Desafio Aleatorio" >
+          <Aleatorio min={1} max={60} />
+        </Card>
 
-      <Card titulo="Titulos" >
-        <h2>Titulo h2</h2>
-        <h3>Titulo h3</h3>
-      </Card>
+        <Card titulo="Titulos" >
+          <h2>Titulo h2</h2>
+          <h3>Titulo h3</h3>
+        </Card>
+
+        <Card titulo="Livros" >
+          <ol>
+            <li>Alice no pais das maravilhas</li>
+            <li>Introduction to algorithms</li>
+            <li>Clean code</li>
+          </ol>
+        </Card>
+
+      </div>
 
     </div>
   );
